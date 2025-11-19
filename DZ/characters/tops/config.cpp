421d52a8c4068291735f16c925bc8c1d7a86b935
Pein <peinfulffa@gmail.com>
2025-11-19 23:03:31 +0000

    Lab Coat
    Added a lab coat with the Helix Corp logo and fancy colours.

--- DZ/characters/tops/config.cpp
+++ DZ/characters/tops/config.cpp
@@ -19,4 +19,23 @@ class cfgVehicles
 {
 	class Clothing_Base;
 	class Clothing;
+	class LabCoat;
+	class Roach_Helix_LabCoat: LabCoat
+	{
+		scope=2;
+		displayName="$STR_CfgVehicles_LabCoat0";
+		descriptionShort="$STR_CfgVehicles_LabCoat1";
+		hiddenSelections[]=
+		{
+			"camoGround",
+			"camoMale",
+			"camoFemale"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"Roach_Movie_Mod\DZ\characters\tops\data\LabCoat_Helix_co.paa",
+			"Roach_Movie_Mod\DZ\characters\tops\data\LabCoat_Helix_co.paa",
+			"Roach_Movie_Mod\DZ\characters\tops\data\LabCoat_Helix_co.paa"
+		};
+	};
 };
